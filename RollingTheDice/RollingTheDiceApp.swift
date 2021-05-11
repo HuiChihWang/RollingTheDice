5054e963416b4213633aa29fc07f961df9695f59
Gilbert <taya87136@.gmail.com>
2021-05-11 14:03:29 +0800

    Initial Commit

---
+++ RollingTheDice/RollingTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingTheDiceApp.swift
+//  RollingTheDice
+//
+//  Created by Hui Chih Wang on 2021/5/11.
+//
+
+import SwiftUI
+
+@main
+struct RollingTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
